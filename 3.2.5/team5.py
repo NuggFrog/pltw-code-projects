9b68c2bb43d09a86747c014600c0c5f698411e7f
NuggFrog <81323207+NuggFrog@users.noreply.github.com>
2023-04-12 11:05:25 -0400

    fixed the code to be good

--- 3.2.5/team5.py
+++ 3.2.5/team5.py
@@ -1,39 +1,26 @@
-'''The function move(my_history, their_history) must return 'r', 'p', or 's'.
-my_history and their_history are strings of the same length, perhaps length zero.
-'''
+# Therefore, this is the best way to win at rock-paper-scissors: 
+# if you lose the first round, switch to the thing that beats the thing your opponent just played. 
+# If you win, don't keep playing the same thing, 
+# but instead switch to the thing that would beat the thing that you just played. 
+# In other words, play the hand your losing opponent just played. 
+# To wit: you win a round with rock against someone else's scissors. 
+# They are about to switch to paper. You should switch to scissors. 
+# Got it? Good.
 
-strategy_name = 'BeatLastMove'
+"""The function move(my_history, their_history) must return 'r', 'p', or 's'.
+my_history and their_history are strings of the same length, perhaps length zero.
+"""
+
+strategy_name = 'W Strategy'
 
 def move(my_history, their_history):
-    '''This player beats its opponent's last move.
-    
-    Returns whatever would beat their last move, either 'r', 'p', or 's'.
-    If there was no last move, returns 'r'.
-    '''
-    if len(their_history)==0:
-        my_move = 'r'
+    if len(my_history) == 0:
+        return 'r'
+    elif their_history[-1] == 'r':
+        return 'p'
+    elif their_history[-1] == 'p':
+        return 's'
+    elif their_history[-1] == 's':
+        return 'r'
     else:
-        prediction = predict_they_will_repeat(their_history.lower())
-        my_move = beat_prediction(prediction)
-    return my_move
-    
-def predict_they_will_repeat(their_history):
-    ''' their_history is a string of length 1 or more.
-    Returns their last move 's', 'r', or 'p'
-    '''
-    return their_history[-1]
-        
-def beat_prediction(prediction):
-    '''prediction is a string of one character: r, p, or s
-    returns 'r', 'p', or 's'
-    '''
-    if prediction =='r':
-        winning_move = 'p'
-    elif prediction == 'p':
-        winning_move = 's'
-    elif prediction == 's':
-        winning_move = 'r'
-    else:
-        winning_move = '' 
-        print ('Error in beat_prediction(): prediction was not r, p, or s.')
-    return winning_move
+        return 'r'
